114dbdcf56dcdab64c56af538fdc7e7428fc7390
Raphael <raphael.silva31@fatec.sp.gov.br>
2019-09-06 19:18:21 -0300

    adjust
    Remove Desc_DishType from Dish class

--- Sln_RestaurantOrder/RestauranteClasses/Dish.cs
+++ Sln_RestaurantOrder/RestauranteClasses/Dish.cs
@@ -9,7 +9,6 @@ namespace RestaurantClasses
         public string Description { get; set; }
         public int Id_Period { get; set; }
         public int Id_DishType { get; set; }
-        public string Desc_DishType { get; set; }
         public bool More_Than_One { get; set; }
 
         public static List<Dish> setDish()
@@ -17,15 +16,15 @@ namespace RestaurantClasses
             List<Dish> lstDish = new List<Dish>();
 
             //Morning
-            lstDish.Add(new Dish() { Id_DishType = 1, Description = "Eggs", Id_Period = 1, Desc_DishType = "Entrée", More_Than_One = false }); //Id_Period - From ID Period List
-            lstDish.Add(new Dish() { Id_DishType = 2, Description = "Toast", Id_Period = 1, Desc_DishType = "Side", More_Than_One = false });
-            lstDish.Add(new Dish() { Id_DishType = 3, Description = "Coffee", Id_Period = 1, Desc_DishType = "Drink", More_Than_One = true });
+            lstDish.Add(new Dish() { Id_DishType = 1, Description = "Eggs", Id_Period = 1,  More_Than_One = false }); //Id_Period - From ID Period List
+            lstDish.Add(new Dish() { Id_DishType = 2, Description = "Toast", Id_Period = 1, More_Than_One = false });
+            lstDish.Add(new Dish() { Id_DishType = 3, Description = "Coffee", Id_Period = 1, More_Than_One = true });
 
             //Night
-            lstDish.Add(new Dish() { Id_DishType = 1, Description = "Steak", Id_Period = 2, Desc_DishType = "Entrée", More_Than_One = false });
-            lstDish.Add(new Dish() { Id_DishType = 2, Description = "Potato", Id_Period = 2, Desc_DishType = "Side", More_Than_One = true });
-            lstDish.Add(new Dish() { Id_DishType = 3, Description = "Wine", Id_Period = 2, Desc_DishType = "Drink", More_Than_One = false });
-            lstDish.Add(new Dish() { Id_DishType = 4, Description = "Cake", Id_Period = 2, Desc_DishType = "Dessert", More_Than_One = false });
+            lstDish.Add(new Dish() { Id_DishType = 1, Description = "Steak", Id_Period = 2, More_Than_One = false });
+            lstDish.Add(new Dish() { Id_DishType = 2, Description = "Potato", Id_Period = 2, More_Than_One = true });
+            lstDish.Add(new Dish() { Id_DishType = 3, Description = "Wine", Id_Period = 2, More_Than_One = false });
+            lstDish.Add(new Dish() { Id_DishType = 4, Description = "Cake", Id_Period = 2, More_Than_One = false });
 
             return lstDish;
         }
